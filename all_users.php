ecb83c07f5c6d29f9f4ec5b895f593e542eefd38
CLEMENT MATEO <clement.mateo@iut.local>
2019-11-20 11:39:19 +0100

    fix #3.1 Filtre sur status et username

--- all_users.php
+++ all_users.php
@@ -1,54 +1,68 @@
 <!DOCTYPE html>
-<html lang="fr">
-	<head>
-		<meta charset="utf-8">
-		<title>Premiers pas avec PDO</title>		
-   </head>
-<?php ?>
-	<body>
-			<?php
-			$host = 'localhost';
-			$db   = 'my_activities';
-			$user = 'root';
-			$pass = 'root';
-			$charset = 'utf8mb4';
-			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-			$options = [
-				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-				PDO::ATTR_EMULATE_PREPARES   => false,
-			];
-			try {
-				$pdo = new PDO($dsn, $user, $pass, $options);
-			} catch (PDOException $e) {
-				throw new PDOException($e->getMessage(), (int)$e->getCode());
-			}
-			$pdo->beginTransaction();
-			$stmt = $pdo->query('SELECT *
-								 FROM users U
-								 JOIN status S ON U.status_id = S.id
-								 WHERE U.status_id = "2"
-								 AND username LIKE "e%"
-								 ORDER BY username');
-			echo '<table>';	
-			echo '<tr><td>Id</td><td>Username</td><td>Email</td><td>Status</td></tr>';
-			while ($row = $stmt->fetch()){
-				echo '<tr><td>'.$row['id'].'</td><td>'.$row['username'].'</td><td>'.$row['email']
-				.'</td><td>'.$row['name'].'</td></tr>';
-			}
-			echo '</table>';
-			
-			// try {
-				// $pdo->beginTransaction();
-				// $stmt = $pdo->prepare("INSERT INTO users (name) VALUES (?)");
-				// foreach (['Lisa','Frederic'] as $name){
-					// $stmt->execute([$name]);
-				// }
-				// $pdo->commit();
-			// }catch (Exception $e){
-				// $pdo->rollBack();
-				// throw $e;
-			// }
-			?>
-	</body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>All users</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+    </style>
+</head>
+<body>
+
+<?php
+$host = 'localhost';
+$port = '3306';
+$db = 'my_activities';
+$user = 'root';
+$pass = 'root';
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    echo $e->getMessage() ;
+    throw new PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
+
+<h1>All Users</h1>
+
+<?php
+$start_letter = 'e';
+$status_id = 2 ;
+$sql = "select users.id as user_id, username, email, s.name as status from users join status s on users.status_id = s.id where username like '$start_letter%' and status_id = $status_id order by username";
+$stmt = $pdo->query($sql);
+?>
+<table>
+    <tr>
+        <th>Id</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Status</th>
+    </tr>
+    <?php while ($row = $stmt->fetch()) { ?>
+    <tr>
+        <td><?php echo $row['user_id']?></td>
+        <td><?php echo $row['username']?></td>
+        <td><?php echo $row['email']?></td>
+        <td><?php echo $row['status']?></td>
+    </tr>
+    <?php } ?>
+</table>
+
+
+</body>
 </html>
